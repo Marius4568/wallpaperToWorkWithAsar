ffba348efa3380a8cced0f1e69076078dacd40e4
Marius Urbonas <marius.urbonas100@gmail.com>
2024-01-21 16:03:52 +0200

    should work with asar

--- source/windows.js
+++ source/windows.js
@@ -1,34 +1,30 @@
-import {promisify} from 'node:util';
-import childProcess from 'node:child_process';
-import path from 'node:path';
-import {fileURLToPath} from 'node:url';
+import { promisify } from "node:util";
+import childProcess from "node:child_process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const execFile = promisify(childProcess.execFile);
 
 // Binary source → https://github.com/sindresorhus/win-wallpaper
-const binary = path.join(__dirname, 'windows-wallpaper-x86-64.exe');
+let binary = path.join(__dirname, "windows-wallpaper-x86-64.exe");
+if (binary.includes("app.asar")) {
+	binary = binary.replace("app.asar", "app.asar.unpacked");
+}
 
 export async function getWallpaper() {
-	const arguments_ = [
-		'get',
-	];
+	const arguments_ = ["get"];
 
-	const {stdout} = await execFile(binary, arguments_);
+	const { stdout } = await execFile(binary, arguments_);
 	return stdout.trim();
 }
 
-export async function setWallpaper(imagePath, {scale = 'fill'} = {}) {
-	if (typeof imagePath !== 'string') {
-		throw new TypeError('Expected a string');
+export async function setWallpaper(imagePath, { scale = "fill" } = {}) {
+	if (typeof imagePath !== "string") {
+		throw new TypeError("Expected a string");
 	}
 
-	const arguments_ = [
-		'set',
-		path.resolve(imagePath),
-		'--scale',
-		scale,
-	];
+	const arguments_ = ["set", path.resolve(imagePath), "--scale", scale];
 
 	await execFile(binary, arguments_);
 }
